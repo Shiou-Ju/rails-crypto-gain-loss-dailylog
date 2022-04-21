0f887e0c8de6c194c13f343506bb930ec35fee26
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-04-21 21:26:00 +0800

    feat: set root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :entries
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'entries#index'
+  # this will reference @entries = Entry.all
+  # # means action
 end
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Defines the root path route ("/")
+# root "articles#index"
